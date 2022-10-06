804841e75aaea1a697bb684e53dc4ca2179f06c8
Joshua Cooper <65969149+joshcooper6@users.noreply.github.com>
2022-10-05 19:06:20 -0700

    commit

--- src/comps/Dashboard.tsx
+++ src/comps/Dashboard.tsx
@@ -9,13 +9,17 @@ import TestPage from "./TestPage";
 import { PATH } from "../confgs";
 import logo from '../assets/logo.png';
 import { Header, LogoutButton, Broadcast, NumCounter, UpdRotating, Reports, TeaInventory } from './';
+import { render } from "@testing-library/react";
 const cookies = new Cookies();
 
 export default function Dashboard(props: any) {
 
     const token = cookies.get("session-token");
     const [numsNeeded, setNumsNeeded] = useState([]);
-    const [greeting, setGreeting] = useState('');
+    const [greeting, setGreeting] = useState({
+        greeting: '',
+        language: ""
+    });
     const [reports, setReports] = useState([{ _id: '', date: '', time: '', user: '', numsReported: [] }]);
     const [rotatingNums, setRotatingNums] = useState([]);
     const [todaysNums, setTodaysNums] = useState({
@@ -148,19 +152,19 @@ export default function Dashboard(props: any) {
             .catch((error) => {
                 console.log('Something went wrong', error);
             });
+
+            const config = {
+                method: 'get',
+                url: 'https://www.greetingsapi.com/random'
+            };
+    
+            axios(config)
+                .then((res) => {
+                    console.log('Random greeting', res.data)
+                    setGreeting(res.data)
+                })
     }, []);    
 
-    useEffect(() => {
-        const config = {
-            method: 'get',
-            url: 'https://www.greetingsapi.com/random'
-        };
-
-        axios(config)
-            .then((res) => {
-                console.log('Random greeting', res.data)
-            })
-    }, []);
 
     const provVals = {
         getFromServer,
@@ -179,24 +183,42 @@ export default function Dashboard(props: any) {
         tea,
         setTea
     };
+
+    const renderNumberAlert = () => {
+        if ((todaysNums.morning.length > 0) && (todaysNums.afternoon.length > 0)) {
+            return 'You will have numbers in the morning and the afternoon'
+        } else if ((todaysNums.morning.length > 0) && (todaysNums.afternoon.length <= 0)) {
+            return 'You will only have numbers in the morning'
+        } else if (todaysNums.morning.length <= 0 && todaysNums.afternoon.length > 0) {
+            return 'You will only have numbers in the afternoon'
+        };
+    };
     
      
 return(<>
-        <div className="flex gap-2 flex-col min-h-screen min-w-screen">
+        <div className="flex gap-2 flex-col justify-between min-h-screen min-w-screen">
             <NumsContext.Provider value={provVals}>
 
-                <div className="w-screen bg-blue-500 p-10 flex justify-around">
-                    <img src={logo} alt="ucl logo" className="h-20" />
-                    <h1></h1>
+                <div className="w-screen p-10 bg-blue-500 flex flex-col sm:flex-row md:justify-around justify-center items-center">
+                    <img src={logo} alt="ucl logo" className="w-1/2 md:w-3/12 max-w-xs" />
+                    <h1 className="pt-10 md:pt-0 text-center text-2xl max-w-sm md:text-3xl self-center text-white">{greeting.greeting} {user.firstName}! <br /> That's Hello in {greeting.language}.</h1>
                 </div> 
 
-                {/* <Header />
-                <LogoutButton />
+                <h1 className="text-center md:text-6xl text-4xl pl-6 pb-6 font-bold uppercase">Dashboard</h1>
+
+{/* 
+                <Header /> */}
+                
+                <div className="flex flex-col self-center w-10/12 justify-center gap-4 overflow-y-scroll">
                 <NumCounter />
-                <UpdRotating />
+                {/* <UpdRotating /> */}
                 <Reports />
-                <Broadcast />
-                <TeaInventory /> */}
+                {/* <Broadcast /> */}
+                <TeaInventory />
+                </div>
+                <div className="text-center mb-6">
+                <LogoutButton />
+                </div>
             </NumsContext.Provider>
         </div>
         
