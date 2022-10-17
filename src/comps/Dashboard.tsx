a7c477dbac5b622a6a8c609d5ddabbe72caa1f6a
Joshua Cooper <65969149+joshcooper6@users.noreply.github.com>
2022-10-17 12:49:45 -0700

    changes

--- src/comps/Dashboard.tsx
+++ src/comps/Dashboard.tsx
@@ -3,10 +3,8 @@ import Cookies from "universal-cookie";
 import { Navigate } from "react-router-dom";
 import axios from "axios";
 import NumsContext from "../context/NumsContext";
-import { ProtectedRoutes } from '../comps';
-import { Routes, Route } from "react-router-dom";
-import TestPage from "./TestPage";
 import { PATH } from "../confgs";
+import logo from '../assets/logo.png';
 import { Header, LogoutButton, Broadcast, NumCounter, UpdRotating, Reports, TeaInventory } from './';
 const cookies = new Cookies();
 
@@ -20,6 +18,7 @@ export default function Dashboard(props: any) {
         email: "",
         exp: ""
     });
+
     const [foodDB, setFoodDB] = useState([]);
 
     const getAll = async () => {
@@ -55,6 +54,7 @@ export default function Dashboard(props: any) {
                 console.log('Something went wrong', error);
             });
     }, []);    
+    
 
     const provVals = {
         getAll,
@@ -68,10 +68,24 @@ export default function Dashboard(props: any) {
 
      
 return(<>
-        <div className="flex gap-2 pb-10 flex-col min-h-screen min-w-screen justify-center align-center">
+        <div className="flex gap-2 pb-10 flex-col min-w-screen justify-center align-center">
             <NumsContext.Provider value={provVals}>
-                <Header />
-                <LogoutButton />
+
+                <div className="bg-blue-500 drop-shadow-xl mb-10 p-6 justify-between self-center text-center w-screen flex flex-row-reverse">
+                    
+                    <div id="HEADER_TEXT" className="self-center flex flex-col">
+                        <h2 className="text-white self-end flex lg:text-4xl text-xl font-black uppercase">Welcome {user?.firstName} <span className="md:flex hidden font-thin">/{user?.username}</span></h2>
+                        <div className="flex flex-col self-end">
+                            <h2 className="text-white text-md lg:text-xl opacity-80 uppercase font-light tracking-widest">Account Role: <span className="font-bold">{user?.role}</span></h2>
+                            <LogoutButton override="hover:font-black hover:opacity-100 opacity-50 self-end text-left text-white font-light uppercase" />
+                        </div>
+                    </div>
+                    
+                    <img className="w-2/12 opacity-70 logo_height self-center max-w-sm" src={logo} alt='ucl logo' />
+                </div>
+
+                {/* <Header /> */}
+                {/* <LogoutButton /> */}
                 <NumCounter />
                 {/* <UpdRotating /> */}
                 <Reports />
--- src/comps/LogoutButton.tsx
+++ src/comps/LogoutButton.tsx
@@ -6,9 +6,10 @@ export default function LogoutButton(props: any) {
    const styles = `text-white uppercase tracking-widest self-center bg-blue-500 p-4 rounded-xl max-w-lg w-4/5`;
    const removeThisCookie = 'session-token';
    const text = props.text || 'Logout';
-   
+   const override_styles = props.override;
+
    return(<>
-        <button className={styles} onClick={() => {
+        <button className={override_styles || styles} onClick={() => {
             cookies.remove(removeThisCookie, { path: "/" });
             window.location.href = "/";
         }}>{text}</button>
