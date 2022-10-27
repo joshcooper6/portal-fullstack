43c3165cde11cfd0194e97ca2ca68ead926668cd
Joshua Cooper <65969149+joshcooper6@users.noreply.github.com>
2022-10-27 08:21:05 -0700

    feature functioning

--- src/comps/TitleChange.js
+++ src/comps/TitleChange.js
@@ -1,22 +1,49 @@
 import { useState, useEffect, useContext } from "react"
 import NumsContext from "../context/NumsContext";
+import axios from "axios";
+import { PATH } from "../confgs";
 
 export default function TitleChange(props) {
    
     const [isActive, setIsActive] = useState(false);
     const [value, setValue] = useState(props.value);
-    const { setNumsNeeded } = useContext(NumsContext);
+    const { getAll, user } = useContext(NumsContext);
     
     function handleClick() {
-        setIsActive(!isActive);
+        {user.role === 'Admin' ? setIsActive(!isActive) : alert('You are not authorized to change the title.')}
     };
+
+    function handleChange(e) {
+        setValue(e.target.value)
+    }
+
+    function handleSubmit() {
+        const config = {
+            method: 'post',
+            url: `${PATH}/updateFoodItem`,
+            data: {
+                query: { id: props.id },
+                changeThis: { name: value }
+            }
+        };
+
+        axios(config)
+            .then((res) => {
+                console.log(`${props.id} was updated to ${value}.`)
+                getAll();
+            })
+            .catch((err) => {
+                console.log('Posting nums went wrong', err)
+            });
+
+    }
 
     function handleBlur() {
         handleClick();
-        
-        if (window.confirm('Are you sure you want to change the name in the DB?')) {
-            alert('This is when they be posted.')
-        };
+        handleSubmit();
+        // if (window.confirm('Are you sure you want to change the name in the DB?')) {
+        //     alert('This is when they be posted.')
+        // };
     }
 
     function Text() {
@@ -24,24 +51,16 @@ export default function TitleChange(props) {
     }
 
     function TextInput() {
-        const hc = (e) => {
-            setValue(e.target.value)
-        }
-
-        return <input onBlur={handleBlur} onChange={hc} type="text" value={value} autoFocus={true} />
+        return <input onBlur={handleBlur} onChange={handleChange} type="text" value={value} autoFocus={true} 
+            className={'w-11/12 p-3 text-2xl font-black bg-white bg-opacity-10 border-0 rounded-full focus:outline-none'}
+        />
     }
 
-    // useEffect(() => {
-    //     setNumsNeeded((prev) => 
-    //         prev.map((object) => {
-    //             if(object.id === props.id) {
-    //                 return {...object, name: value }
-    //             };
-                
-    //             return object;
-    //         })
-    //     );
-    // }, [value]);
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' && isActive) {
+            handleBlur()
+        };
+    })
     
    return <>
         { isActive ? <TextInput /> : <Text /> }
